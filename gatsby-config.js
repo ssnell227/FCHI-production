ca88ed3b5507c52c11b4bab6475e25cdd8b67086
ssnell227 <ssnell227@gmail.com>
2020-10-13 16:07:49 -0600

    makes schedule link dynamic across website

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,6 +41,13 @@ module.exports = {
       },
     },
     {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/misc`,
+        name: 'misc',
+      },
+    },
+    {
       resolve: `gatsby-plugin-sitemap`,
     },
     {
